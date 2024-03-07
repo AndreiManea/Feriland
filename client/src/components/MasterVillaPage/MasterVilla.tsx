90196df94c664b05c16fa6fd99e704c554c3c50a
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-03-07 21:15:32 +0200

    Scroll to top

--- client/src/components/MasterVillaPage/MasterVilla.tsx
+++ client/src/components/MasterVillaPage/MasterVilla.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DetailsSection from './DetailsSection/DetailsSection';
 import HeroSection from './HeroSection/HeroSection';
 
 const MasterVillaPage: React.FC = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+  }, []);
+  
   return (
     <>
       <HeroSection />
