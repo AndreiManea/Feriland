15fbbe8795bffde0600fd3a8e10a7faa8d0213de
Andrei Manea <m.a.maneaandrei.bussiness@gmail.com>
2024-06-28 13:57:15 +0300

    FER-14 Implementation

--- client/src/utils/styles/theme.ts
+++ client/src/utils/styles/theme.ts
@@ -1,6 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 
+const fonts = {
+  heading: `"Kaftan Serif", serif`,
+  body: `"Satoshi", sans-serif`,
+};
+
 const customTheme = extendTheme({
+  fonts,
   colors: {
     primary: {
       500: '#547c39',
