d1a0bd15ce4ac322a1de9d1fd2a4fc80917878f9
Andrei Manea <m.a.maneaandrei.bussiness@gmail.com>
2024-07-24 13:08:44 +0300

    Fixes for FER-3 slider section

---
+++ client/src/utils/data.ts
@@ -0,0 +1,44 @@
+export const slidesData = [
+  {
+    title: 'Wine Fields',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corrupti dolorem, voluptatibus vel quam neque voluptas',
+    imgSrc:
+      'https://assets-global.website-files.com/65bcf5fac0f3634790a816fe/65f048f116bd7c6211cd257d_MOGGI33-2C6A06591.png',
+  },
+  {
+    title: 'Bike Trails',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corrupti dolorem, voluptatibus vel quam neque voluptas',
+    imgSrc:
+      'https://assets-global.website-files.com/65bcf5fac0f3634790a816fe/663cf24436b8616941c44791_1920x1080-as_2022_4756.jpg',
+  },
+  {
+    title: 'Untouched Forest',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corrupti dolorem, voluptatibus vel quam neque voluptas',
+    imgSrc:
+      'https://assets-global.website-files.com/65bcf5fac0f3634790a816fe/663cf411204fd203019f5e7f_7fe0d3194930115(1).660468caa465d.jpeg',
+  },
+  {
+    title: 'Calm & Silence',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corrupti dolorem, voluptatibus vel quam neque voluptas',
+    imgSrc:
+      'https://assets-global.website-files.com/65bcf5fac0f3634790a816fe/663cf29b50394139b3140d3c_5e6c38104157013(1).5f5cc14e15943-p-1600.jpg',
+  },
+  {
+    title: 'Ciubar',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corrupti dolorem, voluptatibus vel quam neque voluptas',
+    imgSrc:
+      'https://assets-global.website-files.com/65bcf5fac0f3634790a816fe/65c4e5162f4b9d4b16b2a0bd_woman-bikini-bathing-japanese-style-bath-outdoors.jpg',
+  },
+  {
+    title: 'Wild Life',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corrupti dolorem, voluptatibus vel quam neque voluptas at placeat dolor ',
+    imgSrc:
+      'https://assets-global.website-files.com/65bcf5fac0f3634790a816fe/65f07d77580e270a5d790676_374581141_686381230189221_533891080229178423_n.jpg',
+  },
+];
--- client/src/utils/styles/theme.ts
+++ client/src/utils/styles/theme.ts
@@ -5,6 +5,24 @@ const fonts = {
   body: `"Satoshi", sans-serif`,
 };
 
+const components = {
+  Text: {
+    baseStyle: {
+      fontSize: '1.2em',
+    },
+  },
+  Heading: {
+    sizes: {
+      h3: {
+        fontSize: '2.625rem', // 42px
+      },
+      h2: {
+        fontSize: '3.5rem', // 56px
+      },
+    },
+  },
+};
+
 const customTheme = extendTheme({
   fonts,
   colors: {
@@ -12,6 +30,7 @@ const customTheme = extendTheme({
       500: '#547c39',
     },
   },
+  components,
 });
 
 export default customTheme;
