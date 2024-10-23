40901c921e2e44d0b33201a61f879a5b924fe9a7
EduardK <eduard.kutnik.dev@gmail.com>
2024-10-23 22:37:56 +0300

    update build

--- server/bin/render-build.sh
+++ server/bin/render-build.sh
@@ -4,8 +4,9 @@ set -o errexit
 
 bundle install
 
+rake
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
 
- bundle exec rails db:migrate
+bundle exec rails db:migrate
