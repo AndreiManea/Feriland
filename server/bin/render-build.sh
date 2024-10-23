a15d463579a73254344656c6a0451d2693cc0f49
EduardK <eduard.kutnik.dev@gmail.com>
2024-10-23 22:45:43 +0300

    Final update

--- server/bin/render-build.sh
+++ server/bin/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 bundle install
 
-rake
+rake db:create_cabins_with_reservations
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
