62da95ff6700fb318aa358f402205274be148e5c
Andrei Manea <m.a.maneaandrei.bussiness@gmail.com>
2024-10-24 16:24:25 +0300

    Add booking dates to master villa as well

--- client/src/components/MasterVillaPage/MasterVilla.tsx
+++ client/src/components/MasterVillaPage/MasterVilla.tsx
@@ -1,10 +1,20 @@
 import React, { useEffect } from 'react';
 import DetailsSection from './DetailsSection/DetailsSection';
 import HeroSection from './HeroSection/HeroSection';
+import { useAppDispatch } from '../../redux/hooks';
+import { setBookedDates } from '../../redux/slices/bookingsDatesSlice';
 
 const MasterVillaPage: React.FC = () => {
+  const dispatch = useAppDispatch();
+
   useEffect(() => {
     window.scrollTo(0, 0);
+    const fetchDates = async () => {
+      const request = await fetch('https://feriland.onrender.com/reservations');
+      const response = await request.json();
+      dispatch(setBookedDates(response.dates));
+    };
+    fetchDates();
   }, []);
 
   return (
